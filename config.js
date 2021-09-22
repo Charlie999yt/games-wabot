794df9b5e5aeecd19bcec0a472dfaf56d33fdad4
Charlie999yt <88020783+Charlie999yt@users.noreply.github.com>
2021-09-22 12:26:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,23 +2,23 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '6281390658325': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+  '919556416715': { // put your number here
+    name: 'Owner Mikey Bot/Creator games-wabot',
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
+  '919556416715': { // put your number here
     name: 'Dave',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '60199782326': {
+  '919556416715': {
     name: 'Adiixyz',
     isReport: true, // if true this number will send if user use command /report or /join
   },
-  '6285713964963': {
+  '919556416715': {
     isReport: true
   },
   '13479805233': {} // ignore if you don't want anything, like name dll
@@ -46,8 +46,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/919556416715'
+global.author = 'Mikey Bot Is The Best'
 
 global.multiplier = 39 // The higher, The harder levelup
 
